236fe8b82c8edb22a7e34f8ff26848fd42abcc37
allysonEscolar <allyson.philipe@escolar.ifrn.edu.br>
2024-09-15 22:08:48 -0300

    criacao da logica de login no crud de jogos

--- controller/JogoController.py
+++ controller/JogoController.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from model.jogo import Jogo
 from data.database import db
+from flask_login import login_required
 
 ControllerJogo = Blueprint('jogo', __name__, url_prefix='/')
 
@@ -12,18 +13,20 @@ def recovery():
 
 # Cadastro
 @ControllerJogo.route('/cadastro', methods=['GET'])
+@login_required
 def cadastroGET():
   return render_template('adicionarJogo.html')
 
 @ControllerJogo.route('/cadastro', methods=['POST'])
+@login_required
 def cadastroPOST():
   nomeJogo = request.form.get("nomeJogo")
   disciplina = request.form.get("disciplina")
   conteudo = request.form.get("conteudo")
   descricao = request.form.get("descricao")
   link = request.form.get("link")
-
-  jogo = Jogo(nomeJogo, disciplina, conteudo, descricao)
+  
+  jogo = Jogo(nomeJogo, disciplina, conteudo, descricao, link)
   db.session.add(jogo)
   db.session.commit()
 
@@ -31,11 +34,13 @@ def cadastroPOST():
 
 
 @ControllerJogo.route('/editar/<int:id>')
+@login_required
 def recovery_id(id):
   jogo = Jogo.query.get(id)
   return render_template('editarJogo.html', jogo=jogo)
 
 @ControllerJogo.route('/editar/<int:id>', methods=["POST"])
+@login_required
 def update(id):
   jogo = Jogo.query.get(id)
   jogo.nomeJogo = request.form.get("nomeJogo")
@@ -50,12 +55,14 @@ def update(id):
   return redirect("/")
 
 @ControllerJogo.route('/apagar/<int:id>')
+@login_required
 def delete(id):
   jogo = Jogo.query.get(id)
   db.session.delete(jogo)
   db.session.commit()
   
   return redirect("/")
+
 
 ### Rotas removidas do app.py. É necessário criar um BluePrint especifica para elas
 
--- controller/UsuarioController.py
+++ controller/UsuarioController.py
@@ -11,26 +11,6 @@ bp_usuario = Blueprint("usuario", __name__, template_folder='templates')
 def login():
     return render_template('login.html')
 
-@bp_usuario.route('/create', methods=['GET', 'POST'])
-def create():
-    if request.method=="GET":
-        return render_template('usuario_create.html')
-
-    if request.method=="POST":
-        nome = request.form['nome']
-        email = request.form['email']
-        senha = request.form['senha']
-        
-      
-        u = Usuario(nome, email, senha)
-        db.session.add(u)
-        db.session.commit()
-        return redirect(url_for('usuario.recovery'))
-    
-def load_user(id):
-    usuario = Usuario.query.get(id)
-    return usuario
-
 @bp_usuario.route('/autenticar', methods=['POST'])
 def autenticar():
     email = request.form['email'] 
